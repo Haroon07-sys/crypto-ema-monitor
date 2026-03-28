709bacfbff2ed7013910ee505285157fc7239198
Haroon07-sys <haroonbaloch1926@gmail.com>
2026-03-28 14:28:55 +0500

    Add files via upload

---
+++ templates/bash.bash
@@ -0,0 +1,7 @@
+cd crypto-ema-monitor
+git init
+git add .
+git commit -m "Complete Binance EMA Monitor"
+git branch -M main
+git remote add origin https://github.com/Haroon07-sys/crypto-ema-monitor.git
+git push -u origin main
